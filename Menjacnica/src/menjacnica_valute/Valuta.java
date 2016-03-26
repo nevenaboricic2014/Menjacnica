ffe439c71f7a2978e621889276944d4945d1ef62
Nevena <boricicnevena95@gmail.com>
2016-03-26 14:07:27 +0100

    Napravljen je paket menjacnica_valute i u njemu klasa Valuta sa atributima,geterima i seterima, ali bez logicke provere za njih.

---
+++ Menjacnica/src/menjacnica_valute/Valuta.java
@@ -0,0 +1,44 @@
+package menjacnica_valute;
+
+public class Valuta {
+	
+	public String naziv;
+	public String skraceniNaziv;
+	public double prdajni;
+	public double srednji;
+	public double kupovni;
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public double getPrdajni() {
+		return prdajni;
+	}
+	public void setPrdajni(double prdajni) {
+		this.prdajni = prdajni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	
+	
+	
+
+}
