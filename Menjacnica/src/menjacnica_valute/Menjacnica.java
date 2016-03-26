57b053516778e94726cf3ce97b43e96fc38dc994
Nevena <boricicnevena95@gmail.com>
2016-03-26 19:03:05 +0100

    U okviru paketa menjacnica_valute napravljena je klsasa Menjacnica koja implementira sve metode interfejsa, ali one su za sada sa paznim telom.

---
+++ Menjacnica/src/menjacnica_valute/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica_valute;
+
+import java.util.GregorianCalendar;
+
+import menjacnica_interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
+
+	@Override
+	public void ubaciKurs(Valuta v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(String naziv, String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
