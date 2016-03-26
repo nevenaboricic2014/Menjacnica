458c54761f288a9ee8846337c4e1ca15644ce0af
Nevena <boricicnevena95@gmail.com>
2016-03-26 19:09:46 +0100

    Implementirane su metode u klasi Menjacnica, u okviru grane implementacijainterfejsa.

--- Menjacnica/src/menjacnica_valute/Menjacnica.java
+++ Menjacnica/src/menjacnica_valute/Menjacnica.java
@@ -1,27 +1,72 @@
 package menjacnica_valute;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica_interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs{
+	
+	LinkedList<Valuta> valute;
+	
+	public Menjacnica() {
+		valute=new LinkedList<Valuta>();
+	}
+
 
 	@Override
 	public void ubaciKurs(Valuta v) {
-		// TODO Auto-generated method stub
+		if(v==null){
+			throw new RuntimeException("Greska");
+		}
+		if(valute.contains(v)){
+			return;
+		}
+		valute.add(v);
+		
 		
 	}
 
 	@Override
 	public void izbrisiKurs(String naziv, String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		if(naziv==null || skraceniNaziv==null || datum==null){
+			return;
+		}
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v=valute.get(i);
+			if(v.getNaziv().equals(naziv) && v.getSkraceniNaziv().equals(skraceniNaziv)){
+				
+				for(int j=0; j<v.kursevi.size(); j++){
+					if(v.kursevi.get(j).getDatum().equals(datum)){
+						v.kursevi.remove(j);
+					}
+				}
+				
+			}
+		}
 		
 	}
 
 	@Override
 	public Kurs vratiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+		if(naziv==null || datum==null){
+			return null;
+		}
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v=valute.get(i);
+			if(v.getNaziv().equals(naziv)){
+				
+				for(int j=0; j<v.kursevi.size(); j++){
+					if(v.kursevi.get(j).getDatum().equals(datum)){
+						return v.kursevi.get(j);
+					}
+				}
+				
+			}
+		}
+		
+	
+	return null;
 	}
 
 }
