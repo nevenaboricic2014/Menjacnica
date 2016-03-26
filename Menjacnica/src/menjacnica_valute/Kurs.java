fb301e943913ba606f1548bed9d3379f95f7c93f
Nevena <boricicnevena95@gmail.com>
2016-03-26 18:28:10 +0100

    U paket menjacnica_valute dodata klasa Kurs sa svojim atributima, geterima i seterima, bez logicke kontrole. U klasi Valuta izmenjeni atributi: umesto prodajni,srednji i kupovni, uvedena je lista kursevi. U obe klase uvedeni toString, hashCode i equals.

---
+++ Menjacnica/src/menjacnica_valute/Kurs.java
@@ -0,0 +1,74 @@
+package menjacnica_valute;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+	private GregorianCalendar datum;
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
--- Menjacnica/src/menjacnica_valute/Valuta.java
+++ Menjacnica/src/menjacnica_valute/Valuta.java
@@ -1,12 +1,16 @@
 package menjacnica_valute;
 
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
 public class Valuta {
 	
 	public String naziv;
 	public String skraceniNaziv;
-	public double prdajni;
-	public double srednji;
-	public double kupovni;
+	public LinkedList<Kurs> kursevi;
+	public Valuta() {
+		kursevi=new LinkedList<Kurs>();
+	}
 	public String getNaziv() {
 		return naziv;
 	}
@@ -19,24 +23,40 @@ public class Valuta {
 	public void setSkraceniNaziv(String skraceniNaziv) {
 		this.skraceniNaziv = skraceniNaziv;
 	}
-	public double getPrdajni() {
-		return prdajni;
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kursevi=" + kursevi + "]";
 	}
-	public void setPrdajni(double prdajni) {
-		this.prdajni = prdajni;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
 	}
-	public double getSrednji() {
-		return srednji;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
 	}
-	public void setSrednji(double srednji) {
-		this.srednji = srednji;
-	}
-	public double getKupovni() {
-		return kupovni;
-	}
-	public void setKupovni(double kupovni) {
-		this.kupovni = kupovni;
-	}
+	
 	
 	
 	
